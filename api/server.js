4f163c8b22b7f903b87788538b3599f48117cdd8
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-25 14:41:38 -0700

    work in progress

--- api/server.js
+++ api/server.js
@@ -17,13 +17,14 @@ const authRouter = require('./auth/auth-router')
  */
 
 const server = express();
-server.use('/api/users', userRouter); 
-server.use('/api/auth', authRouter)
+
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use('/api/users', userRouter); 
+server.use('/api/auth', authRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
